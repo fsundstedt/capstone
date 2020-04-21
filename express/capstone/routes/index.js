5d91910fbadac13799825e680c249d14f23a2b95
Frank Sundstedt <frank.sundstedt@gmail.com>
2020-04-21 17:26:40 -0400

    basic back end working. apps and users now producing json objects to be accessed by front end.

--- express/capstone/routes/index.js
+++ express/capstone/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors');
+const Users = require("../models/users.js");
+const Apps = require("../models/apps.js")
+
+const PORT = 2000;
+const app = express();
+
+app.use(cors())
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app.get('/', function(req, res, next) {
+  res.send('Welcome to API').status(200);
+});
+
+app.get("/users", async function(req, res, next) {
+  const all = await Users.getAllUsers();
+  res.json(all);
+});
+
+app.get("/apps", async function(req, res, next) {
+  const all = await Apps.getAllApplications();
+  res.json(all);
+});
+
+app.listen(PORT, () => {
+console.log(`Server is listening on port: ${PORT}`);
 });
 
 module.exports = router;
