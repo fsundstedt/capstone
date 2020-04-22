2fd1fe6263af925d56dfdca6fb9e3722c2e3d535
Frank Sundstedt <frank.sundstedt@gmail.com>
2020-04-22 10:32:55 -0400

    added input for new user. not tested yet.

--- express/capstone/routes/index.js
+++ express/capstone/routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const cors = require('cors');
-const Users = require("../models/users.js");
-const Apps = require("../models/apps.js")
+const Apps = require("../models/apps.js");
+const AddUser = require("../models/addUser.js");
+const Apps = require("../models/apps.js");
 
 const PORT = 2000;
 const app = express();
@@ -14,14 +15,36 @@ app.get('/', function(req, res, next) {
   res.send('Welcome to API').status(200);
 });
 
+/* Shows all users (will be only visible to admin) */
 app.get("/users", async function(req, res, next) {
   const all = await Users.getAllUsers();
   res.json(all);
 });
 
+/* Shows all public applications */
 app.get("/apps", async function(req, res, next) {
   const all = await Apps.getAllApplications();
   res.json(all);
+});
+
+/* Adds new user */
+app.post("/adduser", async (req, res) => {
+  console.log("req body: ", req.body);
+  const { first_name, last_name, email, user_password, is_admin, contact_me } = req.body;
+  const response = await AddUser.addUser(
+    first_name,
+    last_name,
+    email,
+    user_password,
+    is_admin,
+    contact_me
+  );
+
+  if (response.command === "INSERT" && response.rowCount >= 1) {
+    res.sendStatus(200);
+  } else {
+    res.send("Could not add new user").status(409);
+  }
 });
 
 app.listen(PORT, () => {
